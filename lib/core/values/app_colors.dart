7fce7e81108f8be324ad83dc504a1c28578f4f25
Mahmoud <Sshss@example.comecho>
2026-03-06 14:00:05 +0200

    Add app color constants for UI elements

---
+++ lib/core/values/app_colors.dart
@@ -0,0 +1,84 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color backGroundColor = Color(
+    0xffF9F9F9,
+  ); // Light grey background
+  static const Color primary = Color(
+    0xff02369C,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary10 = Color(
+    0xffCCD7EB,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary20 = Color(
+    0xffabbcde,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary30 = Color(
+    0xff819bce,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary40 = Color(
+    0xff5679bd,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary50 = Color(
+    0xff2c58ad,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary60 = Color(
+    0xff02369c,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary70 = Color(
+    0xff022d82,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary80 = Color(
+    0xff012468,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary90 = Color(
+    0xff011b4e,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary110 = Color(
+    0xff011234,
+  ); // Vibrant blue for buttons and highlights
+  static const Color baseBlack = Color(
+    0xff0F0F0F,
+  ); // Dark grey for primary text
+  static const Color baseBlack10 = Color(
+    0xffcfcfcf,
+  ); // Dark grey for primary text
+  static const Color baseBlack20 = Color(
+    0xffafafaf,
+  ); // Dark grey for primary text
+  static const Color baseBlack30 = Color(
+    0xff878787,
+  ); // Dark grey for primary text
+  static const Color baseBlack40 = Color(
+    0xff5f5f5f,
+  ); // Dark grey for primary text
+  static const Color baseBlack50 = Color(
+    0xff373737,
+  ); // Dark grey for primary text
+  static const Color baseBlack60 = Color(
+    0xff0f0f0f,
+  ); // Dark grey for primary text
+  static const Color baseBlack70 = Color(
+    0xff0d0d0d,
+  ); // Dark grey for primary text
+  static const Color baseBlack80 = Color(
+    0xff0a0a0a,
+  ); // Dark grey for primary text
+  static const Color baseBlack90 = Color(
+    0xff080808,
+  ); // Dark grey for primary text
+  static const Color baseBlack100 = Color(
+    0xff050505,
+  ); // Dark grey for primary text
+  static const Color baseBlack110 = Color(
+    0xff030303,
+  ); // Dark grey for primary text
+  static const Color grey = Color(0xff535353); // Medium grey for secondary text
+  static const Color errorColor = Color(0xffCC1010);
+  //red for error messages
+  static const Color successColor = Color(0xff11CE19);
+  //green for success messages
+  static const Color lightBule = Color(0xffEDEFF3);
+  static const Color lightred = Color(0xffF8D2D2);
+  static const Color placeHolder = Color(0xffA6A6A6);
+}
