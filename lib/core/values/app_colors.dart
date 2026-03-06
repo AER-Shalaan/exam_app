eaf594aa2b8fc18e6c0c8573a9602e20ab497d47
Mahmoud <Sshss@example.comecho>
2026-03-06 14:16:30 +0200

    Fix color codes and adjust color constants for consistency

--- lib/core/values/app_colors.dart
+++ lib/core/values/app_colors.dart
@@ -8,7 +8,7 @@ class AppColors {
     0xff02369C,
   ); // Vibrant blue for buttons and highlights
   static const Color primary10 = Color(
-    0xffCCD7EB,
+    0xffccd7eb,
   ); // Vibrant blue for buttons and highlights
   static const Color primary20 = Color(
     0xffabbcde,
@@ -23,19 +23,19 @@ class AppColors {
     0xff2c58ad,
   ); // Vibrant blue for buttons and highlights
   static const Color primary60 = Color(
-    0xff02369c,
-  ); // Vibrant blue for buttons and highlights
-  static const Color primary70 = Color(
     0xff022d82,
   ); // Vibrant blue for buttons and highlights
-  static const Color primary80 = Color(
+  static const Color primary70 = Color(
     0xff012468,
   ); // Vibrant blue for buttons and highlights
-  static const Color primary90 = Color(
+  static const Color primary80 = Color(
     0xff011b4e,
   ); // Vibrant blue for buttons and highlights
-  static const Color primary110 = Color(
+  static const Color primary90 = Color(
     0xff011234,
+  ); // Vibrant blue for buttons and highlights
+  static const Color primary100 = Color(
+    0xff000b1f,
   ); // Vibrant blue for buttons and highlights
   static const Color baseBlack = Color(
     0xff0F0F0F,
@@ -55,22 +55,20 @@ class AppColors {
   static const Color baseBlack50 = Color(
     0xff373737,
   ); // Dark grey for primary text
+
   static const Color baseBlack60 = Color(
-    0xff0f0f0f,
-  ); // Dark grey for primary text
-  static const Color baseBlack70 = Color(
     0xff0d0d0d,
   ); // Dark grey for primary text
-  static const Color baseBlack80 = Color(
+  static const Color baseBlack70 = Color(
     0xff0a0a0a,
   ); // Dark grey for primary text
-  static const Color baseBlack90 = Color(
+  static const Color baseBlack80 = Color(
     0xff080808,
   ); // Dark grey for primary text
-  static const Color baseBlack100 = Color(
+  static const Color baseBlack90 = Color(
     0xff050505,
   ); // Dark grey for primary text
-  static const Color baseBlack110 = Color(
+  static const Color baseBlack100 = Color(
     0xff030303,
   ); // Dark grey for primary text
   static const Color grey = Color(0xff535353); // Medium grey for secondary text
