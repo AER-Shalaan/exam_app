121612fb683d745dd83cde43e15f15c2427a4c34
Mahmoud <Sshss@example.comecho>
2026-03-06 19:07:41 +0200

    assets_paths

---
+++ lib/core/values/images_paths.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String assetsIconsAdd = "assets/Icons/add.svg";
+
+  static const String assetsIconsAddress = "assets/Icons/address.svg";
+
+  static const String assetsIconsAlarmClock = "assets/Icons/Alarm-clock.svg";
+
+  static const String assetsIconsArrowBack = "assets/Icons/arrow_back.svg";
+
+  static const String assetsIconsCarbonResultDraft =
+      "assets/Icons/carbon_result-draft.svg";
+
+  static const String assetsIconsCategory = "assets/Icons/category.svg";
+
+  static const String assetsIconsCheck = "assets/Icons/check.svg";
+
+  static const String assetsIconsDelete = "assets/Icons/delete.svg";
+
+  static const String assetsIconsDiamond = "assets/Icons/Diamond.svg";
+
+  static const String assetsIconsGift = "assets/Icons/gift.svg";
+
+  static const String assetsIconsGiftOpen = "assets/Icons/Gift_open.svg";
+
+  static const String assetsIconsHeartEyes = "assets/Icons/Heart-eyes.svg";
+
+  static const String assetsIconsHome = "assets/Icons/home.svg";
+
+  static const String assetsIconsLocation = "assets/Icons/location.svg";
+
+  static const String assetsIconsLocationOn = "assets/Icons/location_on.svg";
+
+  static const String assetsIconsLogout = "assets/Icons/logout.svg";
+
+  static const String assetsIconsPerson = "assets/Icons/person.svg";
+
+  static const String assetsIconsPhotoCamera = "assets/Icons/photo_camera.svg";
+
+  static const String assetsIconsRemove = "assets/Icons/remove.svg";
+
+  static const String assetsIconsSearch = "assets/Icons/search.svg";
+
+  static const String assetsIconsShoppingCart =
+      "assets/Icons/shopping_cart.svg";
+
+  static const String assetsIconsSort = "assets/Icons/sort.svg";
+
+  static const String assetsIconsSwitch = "assets/Icons/Switch.svg";
+
+  static const String assetsIconsTranslate = "assets/Icons/translate.svg";
+
+  static const String assetsIconsTulip = "assets/Icons/Tulip.svg";
+
+  static const String assetsIconsTune = "assets/Icons/tune.svg";
+
+  static const String assetsIconsVisibilityOff =
+      "assets/Icons/Visibility Off.svg";
+
+  static const String assetsIconsWarning = "assets/Icons/Warning.svg";
+
+  static const String assetsImagesClock = "assets/Images/clock.svg";
+
+  static const String assetsImagesColorPalette =
+      "assets/Images/Color Palette.svg";
+
+  static const String assetsImagesDraftingTools =
+      "assets/Images/Drafting Tools.svg";
+
+  static const String assetsImagesLanguageTranslator =
+      "assets/Images/Language Translator.svg";
+
+  static const String assetsImagesMath = "assets/Images/math.svg";
+
+  static const String assetsImagesMicroscope = "assets/Images/Microscope.svg";
+
+  static const String assetsImagesProfit = "assets/Images/Profit.svg";
+
+  static const String assetsImagesSandClock = "assets/Images/sand_clock.svg";
+}
