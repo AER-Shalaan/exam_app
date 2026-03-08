c11e52b68125142ff1f319aa7e9b3d60dc24939a
AbdEl-Rahman Shalaan <abdelrahman.shalaan03@gmail.com>
2026-03-08 08:24:48 +0200

    Set up dependency injection with GetIt and Injectable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:exam_app/config/di/di.dart';
 import 'package:exam_app/my_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  configureDependencies();
   runApp(const MyApp());
 }
