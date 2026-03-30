6ed8bd51f775d0e71dfae46241bb22e0aa59e0c3
AbdelRahman Shalan <abdelrahman.shalaan03@gmail.com>
2026-03-30 19:47:22 +0200

    Implement subject fetching and display in Explore view
    * Added `GetSubjects` event and handled subject state in `HomeViewModel`.
    * Integrated `GetAllSubjectsUseCase` into `HomeViewModel`.
    * Created `SubjectCard` widget and updated `ExploreView` to display a list of subjects with loading and error handling.
    * Registered new data sources, repositories, and use cases in DI.
    * Cleaned up unused SVG assets and added `arrow_back.svg`.
    * Adjusted `AppBar` theme surface tint and added new text styles.

--- lib/features/home/presentation/cubit/home_states.dart
+++ lib/features/home/presentation/cubit/home_states.dart
@@ -1,11 +1,25 @@
 import 'package:exam_app/core/enums/home_tab.dart';
+import 'package:exam_app/core/state/base_state.dart';
+import 'package:exam_app/features/home/domain/entities/get_all_subjects_entity.dart';
 
 class HomeStates {
   final HomeTab currentTab;
+  BaseState<GetAllSubjectsEntity> subjectState =
+      BaseState<GetAllSubjectsEntity>();
+  HomeStates({
+    this.currentTab = HomeTab.explore,
+    BaseState<GetAllSubjectsEntity>? subjectState,
+  }) {
+    this.subjectState = subjectState ?? BaseState<GetAllSubjectsEntity>();
+  }
 
-  const HomeStates({this.currentTab = HomeTab.explore});
-
-  HomeStates copyWith({HomeTab? currentTab}) {
-    return HomeStates(currentTab: currentTab ?? this.currentTab);
+  HomeStates copyWith({
+    HomeTab? currentTab,
+    BaseState<GetAllSubjectsEntity>? subjectState,
+  }) {
+    return HomeStates(
+      currentTab: currentTab ?? this.currentTab,
+      subjectState: subjectState ?? this.subjectState,
+    );
   }
 }
--- lib/features/home/presentation/pages/explore_view.dart
+++ lib/features/home/presentation/pages/explore_view.dart
@@ -1,32 +1,82 @@
 import 'package:exam_app/core/values/app_strings.dart';
 import 'package:exam_app/core/values/text_styles.dart';
+import 'package:exam_app/features/home/presentation/cubit/home_events.dart';
+import 'package:exam_app/features/home/presentation/cubit/home_states.dart';
+import 'package:exam_app/features/home/presentation/cubit/home_view_model.dart';
 import 'package:exam_app/features/home/presentation/widgets/search_field.dart';
+import 'package:exam_app/features/home/presentation/widgets/subject_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ExploreView extends StatelessWidget {
+class ExploreView extends StatefulWidget {
   const ExploreView({super.key});
+
+  @override
+  State<ExploreView> createState() => _ExploreViewState();
+}
+
+class _ExploreViewState extends State<ExploreView> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() {
+      if (!mounted) return;
+      context.read<HomeViewModel>().doEvent(GetSubjects());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SearchField(),
-        const SizedBox(height: 40),
-        Text(AppStrings.browseBySubject, style: TextStyles.bodyMedium18),
-        const SizedBox(height: 24),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SearchField(),
+            const SizedBox(height: 40),
+            Text(AppStrings.browseBySubject, style: TextStyles.bodyMedium18),
+            const SizedBox(height: 24),
+          ],
+        ),
+
+        Expanded(
+          child: BlocBuilder<HomeViewModel, HomeStates>(
+            builder: (context, state) {
+              final subjectState = state.subjectState;
+              final subjects = subjectState.data?.subjects ?? [];
+
+              if (subjectState.isLoading) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              if (subjectState.errorMessage != null &&
+                  subjectState.errorMessage!.isNotEmpty) {
+                return Center(
+                  child: Text(
+                    subjectState.errorMessage!,
+                    style: TextStyles.errorText,
+                  ),
+                );
+              }
+
+              if (subjects.isEmpty) {
+                return const Center(child: Text("No subjects found"));
+              }
+
+              return ListView.separated(
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 16),
+                itemCount: subjects.length,
+                itemBuilder: (context, index) {
+                  final subject = subjects[index];
+                  return SubjectCard(icon: subject.icon, name: subject.name);
+                },
+              );
+            },
+          ),
+        ),
       ],
     );
   }
 }
-// FilledButton(
-        //   onPressed: () {
-        //     TokenManager.clearToken();
-        //     Navigator.pushNamedAndRemoveUntil(
-        //       context,
-        //       AppRoutes.loginViewRouteName,
-        //       (route) => false,
-        //     );
-        //   },
-        //   child: Text("logout"),
-        // ),
