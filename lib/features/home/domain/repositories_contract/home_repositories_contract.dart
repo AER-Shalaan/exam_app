dfaf6194669fc24a8bfd76507e168608aa3280e9
AbdelRahman Shalan <abdelrahman.shalaan03@gmail.com>
2026-04-17 21:37:25 +0200

    refactor: change class declarations to use 'abstract interface' for HomeApiClient, HomeDataSourcesContract, and HomeRepositoriesContract

--- lib/features/home/domain/repositories_contract/home_repositories_contract.dart
+++ lib/features/home/domain/repositories_contract/home_repositories_contract.dart
@@ -1,7 +1,7 @@
 import 'package:exam_app/core/network/base_response.dart';
 import 'package:exam_app/features/home/domain/entities/get_all_subjects_entity.dart';
 
-abstract class HomeRepositoriesContract {
+abstract interface  class HomeRepositoriesContract {
   Future<BaseResponse<GetAllSubjectsEntity>> getAllSubjects({
     required String token,
   });
